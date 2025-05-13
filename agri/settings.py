b9ad605317d727e21fb697ce26563b409498290c
arunsarwesh <sarweshwarsarweshwar635@gmail.com>
2025-05-13 11:29:44 +0530

    Refactor CORS settings to remove deprecated configurations and simplify origin handling

--- agri/settings.py
+++ agri/settings.py
@@ -49,27 +49,7 @@ INSTALLED_APPS = [
 # CORS configuration
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    '*',
-]
 
-CORS_ALLOWED_ORIGIN_REGEXES = [
-   '*',
-]
-CORS_ALLOW_METHODS = [
-    "GET",
-    "POST",
-    "PUT",
-    "PATCH",
-    "DELETE",
-    "OPTIONS",
-]
-
-CORS_ALLOW_HEADERS = [
-    "content-type",
-    "authorization",
-    "x-csrftoken",
-]
 CORS_ALLOW_CREDENTIALS = True
 
 MIDDLEWARE = [
