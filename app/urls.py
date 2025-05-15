5839e047bee270d95e7ebeb5b31025a3106d5eae
Balasurya-06 <balasurya9597@gmail.com>
2025-05-15 15:40:43 +0530

    Add GET endpoints for retrieving LandPrep, Transplanting, Fertilizer, Harvest, Packaging, Procurement, and Packing data

--- app/urls.py
+++ app/urls.py
@@ -14,6 +14,15 @@ urlpatterns = [
     path('clear_all_data/', views.clear_all_data, name='clear_all_data'),
     path('get_data/', views.display_data, name='get_data'),
     path('display_data_html/', views.display_data, name='display_data_html'),
+
+    path('get_landprep/', views.get_landprep, name='get_landprep'),
+    path('get_transplanting/', views.get_transplanting, name='get_transplanting'),
+    path('get_fertilizer/', views.get_fertilizer, name='get_fertilizer'),
+    path('get_harverst/', views.get_harverst, name='get_harverst'),
+    path('get_packaging/', views.get_packaging, name='get_packaging'),
+    path('get_procurement/', views.get_procurement, name='get_procurement'),
+    path('get_packing/', views.get_packing, name='get_packing'),
+    
 ]
 
 
